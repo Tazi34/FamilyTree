98e1469a48862dd4fb25fe7e685d87386c99cb6f
Paweł Kasjaniuk <p.kasjaniuk@student.mini.pw.edu.pl>
2020-12-06 06:22:31 +0100

    Handle user token - localstorage, appending to requests

---
+++ FamilyTree/ClientApp/src/components/loginPage/tokenService.ts
@@ -0,0 +1,29 @@
+import Axios from "axios";
+
+const authorizationHeader = "Authorization";
+const tokenLocalStorageKey = "userToken";
+
+const appendAuthorizationTokenToRequests = (token: string) => {
+  Axios.defaults.headers.common[authorizationHeader] = `Bearer ${token}`;
+};
+const removeTokenFromRequestHeaders = () => {
+  Axios.defaults.headers.common[authorizationHeader] = ``;
+};
+
+const addTokenToLocalStorage = (token: string) => {
+  localStorage.setItem(tokenLocalStorageKey, token);
+};
+
+const removeTokenFromLocalStorage = () => {
+  localStorage.removeItem(tokenLocalStorageKey);
+};
+
+export const addAuthorizationToken = (token: string) => {
+  appendAuthorizationTokenToRequests(token);
+  addTokenToLocalStorage(token);
+};
+
+export const removeAuthorizationToken = () => {
+  removeTokenFromLocalStorage();
+  removeTokenFromRequestHeaders();
+};
