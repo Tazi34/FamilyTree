2338fd3d2439dd26c18f5a1aae95a3f9f090f753
Paweł Kasjaniuk <p.kasjaniuk@student.mini.pw.edu.pl>
2020-12-21 08:27:10 +0100

    Add draft.js lib

--- FamilyTree/ClientApp/src/applicationRouting.ts
+++ FamilyTree/ClientApp/src/applicationRouting.ts
@@ -1,6 +1,9 @@
 export const HOME_PAGE_URI = "";
 export const TREE_PAGE_URI = `/tree`;
 export const BLOG_PAGE_URI = "/blog";
+export const CREATE_POST_FORM_PAGE_URI = `${BLOG_PAGE_URI}/posts/create`;
+export const EDIT_POST_FORM_PAGE_URI = `${BLOG_PAGE_URI}/posts/edit`;
+
 export const LOGIN_PAGE_URI = "/login";
 export const REGISTER_PAGE_URI = "/register";
 export const LOGOUT_PAGE_URI = "/logout";
