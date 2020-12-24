f7f11ca17257d15899d4b13eaa00dee130e43cbf
KicunKrzysztof <kicunkrzysztof@gmail.com>
2020-12-24 10:54:07 +0100

    work on search endpoint

---
+++ FamilyTree/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FamilyTree.Services;
+using FamilyTree.Models;
+
+namespace FamilyTree.Controllers
+{
+    //[Route("[controller]")]
+    //[ApiController]
+    //public class SearchController : ControllerBase
+    //{
+    //    private ISearchService searchService;
+    //    public SearchController (ISearchService searchService)
+    //    {
+    //        this.searchService = searchService;
+    //    }
+    //    public 
+    //}
+}
